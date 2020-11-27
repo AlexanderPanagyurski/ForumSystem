38766792755a49a71895dc897c64b230455b20a3
AlexanderPanagyurski <alexander.panagyurski@gmail.com>
2020-11-27 20:36:53 +0200

    Fix datetime visualization

--- Web/ForumSystem.Web/wwwroot/js/site.js
+++ Web/ForumSystem.Web/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(function () {
+    $("time").each(function (i, e) {
+        const dateTimeValue = $(e).attr("datetime");
+        if (!dateTimeValue) {
+            return;
+        }
+
+        const time = moment.utc(dateTimeValue).local();
+        $(e).html(time.format("llll"));
+        $(e).attr("title", $(e).attr("datetime"));
+    });
+});
