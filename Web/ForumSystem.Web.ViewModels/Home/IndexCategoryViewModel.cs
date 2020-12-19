bfabc4b05613804a84ac202f74e1693214ece10a
AlexanderPanagyurski <alexander.panagyurski@gmail.com>
2020-12-19 12:20:54 +0200

    Fix category's description visualization on home page

--- Web/ForumSystem.Web.ViewModels/Home/IndexCategoryViewModel.cs
+++ Web/ForumSystem.Web.ViewModels/Home/IndexCategoryViewModel.cs
@@ -1,9 +1,10 @@
 ﻿namespace ForumSystem.Web.ViewModels.Home
 {
+    using AutoMapper;
     using ForumSystem.Data.Models;
     using ForumSystem.Services.Mapping;
 
-    public class IndexCategoryViewModel : IMapFrom<Category>
+    public class IndexCategoryViewModel : IMapFrom<Category>, IHaveCustomMappings
     {
         public string Title { get; set; }
 
@@ -16,5 +17,14 @@
         public string ImageUrl { get; set; }
 
         public int PostsCount { get; set; }
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<Category, IndexCategoryViewModel>()
+                .ForMember(x => x.Description, options =>
+                {
+                    options.MapFrom(x => (x.Description.Length > 10) ? x.Description.Substring(0, 10) + "..." : x.Description);
+                });
+        }
     }
 }
