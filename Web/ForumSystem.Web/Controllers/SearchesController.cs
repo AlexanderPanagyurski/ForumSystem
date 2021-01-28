71727fe918caa7b788ecc640b43273f89e8f2569
AlexanderPanagyurski <alexander.panagyurski@gmail.com>
2021-01-28 16:22:20 +0200

    Implement AutoComplete Search

---
+++ Web/ForumSystem.Web/Controllers/SearchesController.cs
@@ -0,0 +1,32 @@
+﻿namespace ForumSystem.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using ForumSystem.Data;
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SearchesController : BaseController
+    {
+        private readonly ApplicationDbContext db;
+
+        public SearchesController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        public ActionResult<string[]> Searches()
+        {
+            var responseModel = this.db
+                .Posts
+                .Select(x => x.Title)
+                .ToArray();
+
+            return responseModel;
+        }
+    }
+}
