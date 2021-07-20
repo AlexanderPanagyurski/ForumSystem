b1e7575e5ab517a5acd5a66ea54b0580f4456128
AlexanderPanagyurski <alexander.panagyurski@gmail.com>
2021-07-20 16:47:20 +0300

    Add HomePageTests

---
+++ Tests/ForumSystem.Services.Tests/HomePageTests.cs
@@ -0,0 +1,26 @@
+﻿using ForumSystem.Web;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ForumSystem.Services.Tests
+{
+    public class HomePageTests
+    {
+        [Fact]
+        public async Task HomePageShouldHaveTitle()
+        {
+            var serverFactory = new WebApplicationFactory<Startup>();
+            var client = serverFactory.Server.CreateClient();
+
+            var responseMessage=await client.GetAsync("/Home/Index");
+            var responseAsString = await responseMessage.Content.ReadAsStringAsync();
+            Assert.Contains("<h5>", responseAsString);
+        }
+    }
+}
